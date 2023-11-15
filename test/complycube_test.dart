43bcd825c1b37b1cb6d9bf5eff919514a341e5a0
Muaaz Bin Saeed <muaaz.binsaeed@hotmail.com>
2023-11-15 13:15:04 +0400

    .

--- test/complycube_test.dart
+++ test/complycube_test.dart
@@ -1,36 +1,36 @@
+import 'package:complycube/helpers/complycube_event_codes.dart';
+import 'package:complycube/models/events.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:complycube/complycube.dart';
 import 'package:complycube/complycube_platform_interface.dart';
 import 'package:complycube/complycube_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockComplycubePlatform
-    with MockPlatformInterfaceMixin
-    implements ComplycubePlatform {
+class MockComplycubePlatform with MockPlatformInterfaceMixin implements ComplycubePlatform {
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
 
   @override
-  void addMethodCallHandler() {
-    // TODO: implement addMethodCallHandler
-  }
-
-  @override
-  Future<String?> invoke() {
-    // TODO: implement invoke
-    throw UnimplementedError();
-  }
-
-  @override
   // TODO: implement onComplyCubeEvents
-  Stream get onComplyCubeEvents => throw UnimplementedError();
+  Stream<ComplyCubeEvent> get onComplyCubeEvents => throw UnimplementedError();
 
   @override
-  Future<String?> openCCubeNativeBuild(
-      {required Map<String, dynamic> settings}) {
+  Future<String?> openCCubeNativeBuild({required Map<String, dynamic> settings}) {
     // TODO: implement openCCubeNativeBuild
     throw UnimplementedError();
   }
+
+  @override
+  // TODO: implement onCancelled
+  Stream<ComplyCubeError> get onCancelled => throw UnimplementedError();
+
+  @override
+  // TODO: implement onError
+  Stream<List<ComplyCubeError>> get onError => throw UnimplementedError();
+
+  @override
+  // TODO: implement onSuccess
+  Stream<ComplyCubeResult> get onSuccess => throw UnimplementedError();
 }
 
 void main() {
